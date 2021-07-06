30553e383237c0ff317c354af175c7c33422d901
whoisraa <rairaiatlantica@gmail.com>
2021-07-06 15:38:24 +0700

    tugas 10 cpp

---
+++ tugas10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void perkalian(){
+	cout<<"Selamat Datang ";
+}
+
+void perkalianDua(){
+	cout<<"Di Fungsi Perkalian";
+}
+
+int main(){
+	perkalian();
+	perkalianDua();
+}
